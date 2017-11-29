8dda379f1aae9e77f9f55134fef0faad9869ea2c
somalia_lizet_mercado_villanueva <34046300+Somalia26@users.noreply.github.com>
2017-11-29 09:00:31 -0500

    Add files via upload

---
+++ javascripts/scroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+    
+
+    $(function() {
+    $('.links a').bind('click',function(event){
+        var $anchor = $(this);
+ 
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+        event.preventDefault();
+    });
+});
+
+});
